d95fe7b1b27efb84e2906998e075fbad8375aaef
Paulo Oliveira de Medeiros <39312072+pauloliveiram@users.noreply.github.com>
2023-08-24 13:54:46 -0300

    Automatização dos testes

---
+++ tests/spells.spec.ts
@@ -0,0 +1,10 @@
+import { test, expect } from '@playwright/test';
+
+test('Validar o endpoint GET /spells', async({ request }) => {
+  const response = await request.get('/api/spells');
+  const responseBody = await response.json();
+  expect(response.ok()).toBeTruthy();
+  expect(typeof responseBody[0].id).toBe('string');
+  expect(typeof responseBody[0].name).toBe('string');
+  expect(typeof responseBody[0].description).toBe('string');
+})
